86c347deab61d7c1f21f06296176e39ade1c8cf0
Keshav-babu <keshavbabu742@gmail.com>
2022-07-03 23:26:51 +0530

    changed

--- src/component/products.js
+++ src/component/products.js
@@ -1,15 +1,16 @@
 import data from "../data";
 
+import react from "react";
 
-
-const Product=()=>{
-    return <div>
+function Product(){
+    return <div className="grid-container">
     
         {data.map((pro)=>{
             return <div key={pro.id} className="pro">
                 
-                <img src={pro.url}/>
+                <img src={pro.url}/><br/>
                 <button onClick={()=>{console.log(pro.price)}}>Buy Now</button>
+                
 
             </div>
         })}
